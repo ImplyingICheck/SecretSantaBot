085f96acd6cc70db73318f5c98626db0cc48b787
agnorat1887 <agnorat@gmail.com>
2021-01-26 02:23:10 -0500

    Added special feature and disconnect fight wcwbf

--- santa.py
+++ santa.py
@@ -1,18 +1,66 @@
 import discord
 from discord.ext import commands
 
+from datetime import datetime, timedelta
+
 import sys
 import math
 import random
+from asyncio import sleep
+import subprocess
 
 intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix='>', intents=intents)
+intTime = datetime(2015, 2, 1, 15, 16, 17, 345)
+bot.lastFight = intTime
+bot.spite = True
 
 class Santa:
     Name= ""
     give = -1
     receive = False
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    if member.bot:
+        return
+    server = member.guild
+    disconnects = 0
+    if ((datetime.utcnow() - bot.lastFight)/timedelta(minutes = 1)) <= 5:
+        print("On cooldown")
+        return
+        
+    else:
+        print("Something updated")
+        entries = []
+        async for entry in server.audit_logs(limit=3, action = discord.AuditLogAction.member_disconnect):
+            if (datetime.utcnow() - entry.created_at)/timedelta(minutes = 1) < 1:
+                print("Adding an entry")
+                entries.append(entry)
+                disconnects += entry.extra.count
+
+        #Last disconnect fight was more than 3 minutes ago
+        if disconnects >= 3:
+            if before.channel != None:
+                channel = before.channel
+            elif after.channel != None:
+                channel = after.channel
+            bot.lastFight = datetime.utcnow()
+            await wcwbf(channel)
+    
+async def wcwbf (channel):
+    voice_channel = channel
+    channel = None
+    if voice_channel != None:
+        channel = voice_channel.name
+        vc = await voice_channel.connect()
+        song = discord.FFmpegOpusAudio(source="wcwbf.mp3")
+        print(song.is_opus())
+        vc.play(song)
+        while vc.is_playing():
+            await sleep(1)
+        await vc.disconnect()
 
 def checkDone(santas):
     count = 0
@@ -36,6 +84,27 @@ def reset(santas):
         i.receive = False
     return
 
+
+@bot.event
+async def on_ready():
+    print("Bot is ready to bot it up")
+
+@bot.event
+async def on_message(message):
+    if message.author.id == 193508802073460736 and message.content == '-skip' and bot.spite:
+        await message.author.move_to(None)
+        await message.channel.send("Stop skipping tracks bitch")
+
+    await bot.process_commands(message)
+
+@bot.command(pass_context = True)
+async def spite(ctx):
+    if bot.spite and server.owner.name == ctx.author.name:
+        bot.spite = False
+        ctx.author.send("Kempke is no longer being spited")
+    elif not bot.spite and server.owner.name == ctx.author.name:
+        bot.spite = True
+        ctx.author.send("Kempke is now being spited")
 
 @bot.command(pass_context=True)  
 async def santa(ctx, *args):
@@ -84,4 +153,4 @@ async def santa(ctx, *args):
     #await members[0].send("test")
     #await channel.send(f"Users in {role_id}, {santas}")
 
-bot.run(str(sys.argv[1]))
+bot.run(str(sys.argv[1]), bot=True)
