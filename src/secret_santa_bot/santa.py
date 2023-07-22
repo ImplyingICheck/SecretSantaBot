bca487dbc52573ed821af76b1fa9dcaf16747695
Andrew <amhector@stanford.edu>
2023-07-22 16:42:41 -0700

    chore: Setup project files
    test: Add configuration for skipping slow tests
    
    chore: PyCharm: Add pytest run configurations
    
    Skip all tests marked with pytest.mark.slow
    Run all tests, including slow
    
    chore: PyCharm: Remove highly volatile file from version control
    
    build: Remove yapf, add pyink
    
    build: Add explicit check for virtual environment in dev setup script
    
    build: Update pyproject.toml
    
    Add default settings to various code health related packages.
    
    Update .pylintrc
    
    Remove incorrect copyright information
    
    Update README.md
    
    chore: Add template files

---
+++ src/secret_santa_bot/santa.py
@@ -0,0 +1,194 @@
+import discord
+from discord.ext import commands
+
+from datetime import datetime, timedelta
+
+import sys
+import math
+import random
+from asyncio import sleep
+import subprocess
+
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='>', intents=intents)
+intTime = datetime(2015, 2, 1, 15, 16, 17, 345)
+bot.lastFight = intTime
+bot.spite = True
+
+class Santa:
+    Name= ""
+    give = -1
+    receive = False
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    if member.bot:
+        return
+    server = member.guild
+    disconnects = 0
+    if ((datetime.utcnow() - bot.lastFight)/timedelta(minutes = 1)) <= 5:
+        #print("On cooldown")
+        return
+        
+    else:
+        entries = []
+        async for entry in server.audit_logs(limit=3, action = discord.AuditLogAction.member_disconnect):
+            if (datetime.utcnow() - entry.created_at)/timedelta(minutes = 1) < 1:
+                #print("Adding an entry")
+                entries.append(entry)
+                disconnects += entry.extra.count
+
+        #Last disconnect fight was more than 3 minutes ago
+        if disconnects >= 3:
+            if before.channel != None:
+                channel = before.channel
+            elif after.channel != None:
+                channel = after.channel
+            bot.lastFight = datetime.utcnow()
+            await wcwbf(channel)
+
+def is_connected(ctx):
+    voice_client = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+    return voice_client and voice_client.is_connected()
+
+async def wcwbf (channel):
+    voice_channel = channel
+    channel = None
+        
+    if voice_channel != None:
+        channel = voice_channel.name
+        vc = await voice_channel.connect()
+        song = discord.FFmpegOpusAudio(source="wcwbf.mp3")
+        print(song.is_opus())
+        vc.play(song)
+        while vc.is_playing():
+            await sleep(1)
+        await vc.disconnect()
+    
+async def wcwbfctx (ctx, channel):
+    voice_channel = channel
+    channel = None
+    if(is_connected(ctx)):
+        await ctx.channel.send("Bot is already connected")
+        return
+        
+    if voice_channel != None:
+        channel = voice_channel.name
+        vc = await voice_channel.connect()
+        song = discord.FFmpegOpusAudio(source="wcwbf.mp3")
+        print(song.is_opus())
+        vc.play(song)
+        while vc.is_playing():
+            await sleep(1)
+        await vc.disconnect()
+
+
+
+def checkDone(santas):
+    count = 0
+    last = -1
+    for x, i in enumerate(santas):
+        if(i.receive == False):
+            last = x
+            count +=1
+    if count == 1 and (santas[last].give == -1 and santas[last].receive == False):
+        reset(santas)
+        return False
+    elif count >= 1:
+        return False
+    else:
+        return True
+
+def reset(santas):
+    print("RESETTING")
+    for i in santas:
+        i.give = -1
+        i.receive = False
+    return
+
+
+@bot.event
+async def on_ready():
+    print("Bot is ready to bot it up")
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    spiting = message.author.id == 193508802073460736 and message.content == '-skip' and bot.spite
+    if spiting:
+        print("moving")
+        await message.channel.send("Stop skipping tracks bitch")
+        await message.author.move_to(None)
+        
+
+    await bot.process_commands(message)
+
+@bot.command(pass_context = True)
+async def spite(ctx):
+    if bot.spite and ctx.guild.owner.name == ctx.author.name:
+        bot.spite = False
+        await ctx.channel.send("Kempke is no longer being spited")
+    elif not bot.spite and ctx.guild.owner.name == ctx.author.name:
+        bot.spite = True
+        await ctx.channel.send("Kempke is now being spited")
+
+@bot.command(pass_context = True)
+async def play(ctx):
+    server=ctx.guild
+    if(server.owner.name ==  ctx.author.name):
+        if(ctx.author.voice.channel == None):
+            await ctx.channel.send("Please join a voice channel")
+        else:
+            await wcwbfctx(ctx, ctx.author.voice.channel)
+
+@bot.command(pass_context=True)  
+async def santa(ctx, *args):
+    server = ctx.guild
+    channel = ctx.channel
+    if(server.owner.name == ctx.author.name):
+        role_name =(' '.join(args))
+        role_id = server.roles[0]
+        for role in server.roles:
+            if role_name.lower() == role.name.lower():
+                role_id = role
+                break
+
+        else:
+            await channel.send(f"{role_name} Role does not exist")
+            return
+
+        members = role_id.members
+        names = []
+        
+        for member in members:
+            names.append(member.name)
+
+
+        santas = [Santa() for i in range(len(names))]
+        for i in range(0, len(santas)):
+            santas[i].Name = names[i]
+
+        while(not checkDone(santas)):
+            giver = random.randint(0, len(santas)-1)                #Pick a random person to give a gift
+            if(santas[giver].give != -1):                           #Check if that person is already giving a gift
+                continue
+            receiver = random.randint(0, len(santas)-1)           #Pick a random person to receive a gift
+            while(santas[receiver].receive == True or receiver == giver):
+                receiver = random.randint(0, len(santas)-1)
+            santas[receiver].receive = True
+            santas[giver].give = receiver
+
+        for x, i in enumerate(santas):
+            await members[x].send(f"You will be giving a gift to {santas[i.give].Name}\nNote that this is their discord name and not the name that will be within the rules and information file\nThe rules and information for this year along with the addresses of everyone will be in the Secret Santa thread within the {server.name} server. The thread may close after a time but can still be viewed by clicking on the threads button -> Archived -> Private")
+
+            #print("Giver: " + santas[giver].Name + " Receiver: " + santas[receiver].Name)
+        
+    else:
+        await channel.send("Only the owner can roll for secret santa")
+    #await members[0].send("test")
+    #await channel.send(f"Users in {role_id}, {santas}")
+
+bot.run(str(sys.argv[1]), bot=True)
