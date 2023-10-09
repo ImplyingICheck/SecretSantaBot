d341ffe53cc7954d6a368f73e39dbeeea36ec0a1
Andrew <amhector@stanford.edu>
2023-10-09 09:44:19 -0700

    refactor: Move logic into a main function for variable scoping

--- src/secret_santa_bot/main.py
+++ src/secret_santa_bot/main.py
@@ -1,26 +1,20 @@
 """Sets enviornment variables for secret_santa_bot. Notably, reading the token
 used by the bot."""
 import os
-import pathlib
 
-import secret_santa_bot.bot.santa
 from secret_santa_bot import _vault
 
 
-def _get_secret_santa_bot_path() -> pathlib.Path:
-    file_depth = 1
-    root_directory = pathlib.Path(__file__).parents[file_depth]
-    secret_santa_bot_script = root_directory.joinpath(
-        'secret_santa_bot', 'santa.py'
+def main():
+    vault: _vault.Vault = _vault.Vault(
+        url=os.environ['VAULT_PATH'], token=os.environ['VAULT_TOKEN']
     )
-    return secret_santa_bot_script
+    os.environ['GUILD_ID'] = vault.read_secret_token(username='GUILD_ID')
+    discord_authentication_token: str = vault.read_secret_token()
+    import secret_santa_bot.bot.santa
+
+    secret_santa_bot.bot.santa.main(discord_authentication_token)
 
 
 if __name__ == '__main__':
-    vault: _vault.Vault = _vault.Vault(
-        url='http://localhost:8200', token=os.environ['VAULT_TOKEN']
-    )
-    os.environ['GUILD_ID'] = vault.read_secret_token(username='GUILD_ID')
-    script_path: pathlib.Path = _get_secret_santa_bot_path()
-    discord_authentication_token: str = vault.read_secret_token()
-    secret_santa_bot.bot.santa.main(discord_authentication_token)
+    main()
