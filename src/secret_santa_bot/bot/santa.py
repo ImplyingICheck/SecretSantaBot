2c32263b78bdb01b6aa0d600df350529202b5d20
Andrew <amhector@stanford.edu>
2023-10-11 21:48:28 -0700

    feat: Make messaging asynchronous

--- src/secret_santa_bot/bot/santa.py
+++ src/secret_santa_bot/bot/santa.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
+import asyncio
+import logging
 import os
-from collections.abc import Iterable
 from typing import Any
 
 import discord
@@ -44,21 +47,40 @@ async def on_ready() -> None:
     print("Bot is ready to bot it up")
 
 
-async def message_santas(santas: Iterable[Santa]) -> None:
-    for santa in santas:
-        member = santa.member
-        await member.send(
+async def message_santa(santa: Santa) -> bool:
+    successful = False
+    try:
+        await santa.member.send(
             (
                 f"You will be giving a gift to {santa.target}\nNote "
                 f"that this is their discord name and not the name that will "
                 f"be within the rules and information file\nThe rules and "
                 f"information for this year along with the addresses of "
                 f"everyone will be in the Secret Santa thread within the "
-                f"{member.guild.name} server. The thread may close after a time"
-                f" but can still be viewed by clicking on the threads button ->"
-                f" Archived -> Private"
+                f"{santa.member.guild.name} server. The thread may close after "
+                f"a time but can still be viewed by clicking on the threads "
+                f"button -> Archived -> Private"
             )
         )
+        successful = True
+    except discord.errors.Forbidden:
+        logging.error(f"Unable to message '{santa.member}'. The user may have "
+                      f"messaging from non-friend server memebers disabled.")
+    except AttributeError:
+        # Bot attempts to message itself
+        successful = True
+    return successful
+
+def create_santas(role: discord.Role) -> list[Santa]:
+    santas = [Santa(member) for member in role.members]
+    santas.sort(key=lambda santa: santa.member.id)
+    return santas
+
+async def message_santas(role: discord.Role):
+    santas = create_santas(role)
+    santas = chains_of_primes.assign_santas(santas)
+    messages = [message_santa(santa) for santa in santas]
+    return await asyncio.gather(*messages)
 
 
 @bot.tree.command(guild=discord.Object(id=os.environ["GUILD_ID"]))
@@ -70,13 +92,15 @@ async def santa(interaction: discord.Interaction, role: discord.Role):
         guild_owner = object()
     command_invoker = interaction.user.id
     if guild_owner == command_invoker:
-        santas = [Santa(member) for member in role.members]
-        santas.sort(key=lambda santa: santa.member.id)
-        santas = chains_of_primes.assign_santas(santas)
-        await message_santas(santas)
+        message_successful = await message_santas(role)
+        if not all(message_successful):
+            response_message = "Santas successfully messaged."
+        else:
+            response_message = "Error during operation. Check log for details."
+        await interaction.response.send_message(response_message, ephemeral=True)
     else:
         await interaction.response.send_message(
-            "Only the owner can roll for secret santa"
+            "Only the owner can roll for secret santa", ephemeral=True
         )
 
 
