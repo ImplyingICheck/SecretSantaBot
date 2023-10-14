5678dd2181458e340e0b64a7e998449144864233
Andrew <amhector@stanford.edu>
2023-10-14 07:00:28 -0700

    feat: Implement Bot, an extension class of hikari.GatewayBot

--- src/secret_santa_bot/bot/santa.py
+++ src/secret_santa_bot/bot/santa.py
@@ -4,33 +4,90 @@ from __future__ import annotations
 
 import asyncio
 import logging
+import typing
 
 import hikari
 
 from secret_santa_bot.bot import chains_of_primes
 
+if typing.TYPE_CHECKING:
+    import concurrent.futures
+    import os
+    from hikari.internal import data_binding
+    from hikari import intents as intents_
+    from hikari.impl import config as config_impl
+
+
 _logger = logging.getLogger(__name__)
 _logger.handlers = logging.getLogger('hikari').handlers
 
 
+def get_super_dir(object_: object):
+    super_attributes = set()
+    for base_class in object_.__class__.__bases__:
+        super_attributes.update(dir(base_class))
+    return super_attributes
 
-# TODO: Find minimum set of permissions required
-class Bot(commands.Bot):
-    """Sets "/" and default intents required by SecretSantaBot"""
+
+class Bot(hikari.GatewayBot):
+    """A helper class designed to register all bot commands of the lowest
+    subclass. The registration invokes self.listen() on each public method
+    (i.e., any method not prepended with _ or __).
+
+    This class should not be instantiated on its own."""
 
     def __init__(
         self,
-        command_prefix: commands.bot.PrefixType[commands.bot.BotT] | None = None,  # type: ignore # Ignored due to a bug in type hints of discord.py # pylint: disable=line-too-long
+        token: str,
         *,
-        intents: discord.Intents | None = None,
-        **options: Any,
+        allow_color: bool = True,
+        banner: typing.Optional[str] = 'hikari',
+        suppress_optimization_warning: bool = False,
+        executor: typing.Optional[concurrent.futures.Executor] = None,
+        force_color: bool = False,
+        cache_settings: typing.Optional[config_impl.CacheSettings] = None,
+        http_settings: typing.Optional[config_impl.HTTPSettings] = None,
+        dumps: data_binding.JSONEncoder = data_binding.default_json_dumps,
+        loads: data_binding.JSONDecoder = data_binding.default_json_loads,
+        intents: intents_.Intents = intents_.Intents.ALL_UNPRIVILEGED,
+        auto_chunk_members: bool = True,
+        logs: typing.Union[
+            None, str, int, typing.Dict[str, typing.Any], os.PathLike[str]
+        ] = 'INFO',
+        max_rate_limit: float = 300.0,
+        max_retries: int = 3,
+        proxy_settings: typing.Optional[config_impl.ProxySettings] = None,
+        rest_url: typing.Optional[str] = None,
     ):
-        if not command_prefix:
-            command_prefix = '/'
-        if not intents:
-            intents = discord.Intents.default()
-            intents.members = True
-        super().__init__(command_prefix, intents=intents, **options)
+        super().__init__(
+            token,
+            allow_color=allow_color,
+            banner=banner,
+            suppress_optimization_warning=suppress_optimization_warning,
+            executor=executor,
+            force_color=force_color,
+            cache_settings=cache_settings,
+            http_settings=http_settings,
+            dumps=dumps,
+            loads=loads,
+            intents=intents,
+            auto_chunk_members=auto_chunk_members,
+            logs=logs,
+            max_rate_limit=max_rate_limit,
+            max_retries=max_retries,
+            proxy_settings=proxy_settings,
+            rest_url=rest_url,
+        )
+        self.register_commands()
+
+    def register_commands(self):
+        super_dir = get_super_dir(self)
+        local_methods = [
+            method for method in dir(self) if method not in super_dir
+        ]
+        for method in local_methods:
+            if not method.startswith('_'):
+                self.listen()(getattr(self, method))
 
 
 bot = Bot()
