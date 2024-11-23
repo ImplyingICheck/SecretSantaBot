5bc5b12ad2de58136fb8e838e94752600e4cd87b
ImplyingICheck <implyingicheck@proton.me>
2024-11-23 11:47:00 -0800

    build(): Remove --dev option from Hashicorp Vault
    Rework compose.yaml to handle hashicorp vault with persistent storage

---
+++ dev/docker/workflow_vault.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env ash
+
+# Start vault
+vault server -config "$VAULT_CONFIG" &
+sleep 3
+[ ! -e generated_keys.txt ] && vault operator init > generated_keys.txt
+
+sleep 3
+# Parse unsealed keys
+keys="$(awk -F': ' '/Unseal Key [0-9]+:/ {print $2}' generated_keys.txt)"
+for key in $keys; do
+    vault operator unseal "$key"
+done
+
+# Get root token
+rootToken="$(awk -F': ' '/Initial Root Token:/ {print $2}' generated_keys.txt)"
+[ ! -e /secrets/root_token.txt ] && echo "$rootToken" > /runtime_secrets/root_token.txt
+
+# Enable kv secrets
+vault login "$rootToken"
+vault secrets enable -path=secret kv
+
+wait
